3bed169606b06ad84701c0424ca37b6acb1e7902
Stephanie Szeto <stephaniexszeto@gmail.com>
2013-11-18 22:26:10 -0800

    initial code for deadlock detection, compiles

--- src/java/simpledb/LockManager.java
+++ src/java/simpledb/LockManager.java
@@ -1,28 +1,63 @@
 package simpledb;
 
-
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class LockManager {
     private ConcurrentHashMap<PageId, TransactionId> writeMap;
     private ConcurrentHashMap<PageId, ArrayList<TransactionId>> readMap;
+    private ConcurrentHashMap<TransactionId, ArrayList<PageId>> locks;
+    private ConcurrentHashMap<TransactionId, Timestamp> startTimes;
     private static final int TIMEOUT = 100;
+    private static final long TRANSACTION_TIME = 100;
 
     public LockManager() {
         writeMap = new ConcurrentHashMap<PageId, TransactionId>();
         readMap = new ConcurrentHashMap<PageId, ArrayList<TransactionId>>();
+	locks = new ConcurrentHashMap<TransactionId, ArrayList<PageId>>();
+	startTimes = new ConcurrentHashMap<TransactionId, Timestamp>();
     }
 
-    public synchronized void addReadLock(TransactionId tid, PageId pid) throws InterruptedException{
-        if (writeMap.containsKey(pid) && writeMap.get(pid) == tid) {
+    private void checkDeadlock(TransactionId tid) throws TransactionAbortedException {
+	Date date = new Date();
+	Timestamp now = new Timestamp(date.getTime());
+	if (startTimes.containsKey(tid)) {
+	    Timestamp start = startTimes.get(tid);
+	    long startTime= start.getTime();
+	    long currentTime = now.getTime();
+	    if (currentTime - startTime > TRANSACTION_TIME) {
+		if (locks.containsKey(tid)) {
+		    ArrayList<PageId> pages = locks.get(tid);
+		    for (PageId pid : pages) {
+			Database.getBufferPool().releasePage(tid, pid);
+		    }
+		}
+		throw new TransactionAbortedException(); 
+	    }
+	} else {
+	    startTimes.put(tid, now);
+	}
+    }
+
+    public synchronized void addReadLock(TransactionId tid, PageId pid) throws InterruptedException, TransactionAbortedException {
+	checkDeadlock(tid); 
+
+	// adding pid to locks, unsure about placement
+	ArrayList<PageId> pages = new ArrayList<PageId>();
+        if (locks.containsKey(tid)) 
+	    pages = locks.get(tid);
+	pages.add(pid);
+	locks.put(tid, pages);       
+
+	if (writeMap.containsKey(pid) && writeMap.get(pid) == tid) {
             ArrayList<TransactionId> transList = readMap.get(pid);
             if(transList == null) {
                 transList = new ArrayList<TransactionId>();
                 transList.add(tid);
                 readMap.put(pid, transList);
-            }
-            else if(!transList.contains(tid)) {
+            } else if(!transList.contains(tid)) {
                 transList.add(tid);
                 readMap.put(pid, transList);
             }
@@ -52,7 +87,16 @@ public class LockManager {
 
     }
 
-    public synchronized void addWriteLock(TransactionId tid, PageId pid) throws InterruptedException{
+    public synchronized void addWriteLock(TransactionId tid, PageId pid) throws InterruptedException, TransactionAbortedException {
+	checkDeadlock(tid); 
+
+	// adding pid to locks, unsure about placement
+	ArrayList<PageId> pages = new ArrayList<PageId>();
+        if (locks.containsKey(tid)) 
+	    pages = locks.get(tid);
+	pages.add(pid);
+	locks.put(tid, pages); 
+
         if (readMap.containsKey(pid) && readMap.get(pid).contains(tid) && readMap.get(pid).size() == 1) {
             writeMap.put(pid, tid);
         }
@@ -73,6 +117,11 @@ public class LockManager {
     }
 
     public synchronized void removeReadLock(TransactionId tid, PageId pid) {
+	if (locks.containsKey(tid)) {
+	    ArrayList<PageId> pages = locks.get(tid);
+	    pages.remove(pid);
+	}
+
         ArrayList<TransactionId> transList = readMap.get(pid);
         transList.remove(tid);
         readMap.put(pid, transList);
@@ -80,6 +129,11 @@ public class LockManager {
     }
 
     public synchronized void removewriteLock(TransactionId tid, PageId pid) {
+	if (locks.containsKey(tid)) {
+	    ArrayList<PageId> pages = locks.get(tid);
+	    pages.remove(pid);
+	}
+
         writeMap.remove(pid);
         //System.out.println("WRITE LOCK REMOVED BY: " + tid.getId() );
     }
